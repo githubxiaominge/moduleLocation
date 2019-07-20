5fd951e5db44575cbc14fa720671b88b337e3c8c
小敏哥 <284711103@qq.com>
2019-07-20 15:48:31 +0800

    初始化

---
+++ child/child2/index.js
@@ -0,0 +1,6 @@
+var nameByJson = require('nameByJson');
+var parent=require('parent');
+var root =require('root');
+nameByJson();//输出nameByJson
+parent();//输出parent
+root();//输出root
---
+++ index.js
@@ -0,0 +1,2 @@
+var root = require('./node_modules/root');
+root();//输出rootá
